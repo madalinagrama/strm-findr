f978736d6de6f4310e857423e6a759b45d054cdc
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2021-11-24 20:11:51 +0200

    user profile page fixed

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,14 +4,19 @@ import { useAtom } from "jotai";
 import state from "../stateManager";
 import Card from "./Card";
 
-const Index = () => {
+const Index = ({ onlyFavorites = false }) => {
     const [cards] = useAtom(state.cardsAtom);
+    const [favorites] = useAtom(state.favoritesAtom);
 
     return (
         <div className="row">
-            {cards.map((card, i) => (
-                <Card key={i} {...card} handleFavorites={true} />
-            ))}
+            {cards
+                .filter((c) =>
+                    onlyFavorites ? favorites.includes(c.id) : true
+                )
+                .map((card, i) => (
+                    <Card key={i} {...card} />
+                ))}
         </div>
     );
 };
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,8 +1,9 @@
-import {useAtom} from "jotai";
-import {useState, useEffect} from "react";
+import { useAtom } from "jotai";
+import { useState, useEffect } from "react";
 import state from "../stateManager";
-import {getUser} from "./user/service/UserService";
+import { getUser } from "./user/service/UserService";
 import profile from "../img/profile.jpg";
+import Index from "./Index";
 
 const UserProfile = () => {
     const profileProps = {
@@ -12,25 +13,23 @@ const UserProfile = () => {
     };
 
     const [currentUser, setCurrentUser] = useAtom(state.currentUserAtom);
-    const [favorites, setFavorites] = useAtom(state.favoritesAtom);
-    console.log(currentUser);
+
     const [user, setUser] = useState({
         username: null,
         email: null,
         joinedDate: null,
     });
-    console.log(user);
 
     useEffect(() => {
         const setTheUser = async () => {
-            return getUser(currentUser).then((res) => {
+            return getUser(currentUser.username).then((res) => {
                 setUser(res.data);
                 setCurrentUser(res.data);
             });
         };
 
         setTheUser();
-    }, []); // de adaugat favorites in acest deps
+    }, []);
 
     return (
         <div className="card my-3">
@@ -38,16 +37,15 @@ const UserProfile = () => {
                 <h5>Your Profile</h5>
             </div>
             <div>
-                <img {...profileProps} alt={user.username}/>
+                <img {...profileProps} alt={user.username} />
             </div>
             <div className="card-body">
-                    <h5 className="card-title">Name: {user.username}</h5>
-                    <p> </p>
-                    <p className="card-text">Email: {user.email}</p>
-                    <p className="card-text">Joined date: {user.joinedDate}</p>
-                     {/*/!*map ptr fiecare movieId*!/ de verificat sa nu fie null obiectul pe care se face maparea */}
-                {/*{favorites?.map()}*/}
+                <h5 className="card-title">Name: {user.username}</h5>
+                <p> </p>
+                <p className="card-text">Email: {user.email}</p>
+                <p className="card-text">Joined date: {user.joinedDate}</p>
             </div>
+            <Index onlyFavorites={true} />
         </div>
     );
 };
